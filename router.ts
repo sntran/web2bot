44a41895f944cda9e59205627914b33663b631a5
Son Tran-Nguyen <github@sntran.com>
2022-05-01 22:20:32 -0500

    refactor(Router): manually register commands
    Using `fetch`, instead of an external module.

--- router.ts
+++ router.ts
@@ -10,7 +10,6 @@ import { router } from "https://crux.land/router@0.0.11";
 import nacl from "https://cdn.skypack.dev/tweetnacl@v1.0.3?dts";
 
 import {
-  DiscordInteractions,
   Snowflake,
   PartialApplicationCommand,
   ApplicationCommand,
@@ -20,7 +19,9 @@ import {
   InteractionType,
   ApplicationCommandInteractionData,
   InteractionResponseType,
-} from "https://deno.land/x/discord_slash_commands@1.0.6/src/index.ts";
+} from "https://deno.land/x/discord_slash_commands@1.0.6/src/structures/index.ts";
+
+const DISCORD_BASE_URL = "https://discord.com/api/v9";
 
 const NAME_REGEX = /^[-_\p{L}\p{N}\p{sc=Deva}\p{sc=Thai}]{1,32}$/u;
 
@@ -30,7 +31,11 @@ export type Handler = (
   params: Record<string, string>,
 ) => Response | Promise<Response>;
 
-export class Router extends DiscordInteractions {
+export class Router {
+  #publicKey: Snowflake;
+  #applicationId: Snowflake;
+  #authToken: string;
+  #tokenPrefix: string;
   #commands: Record<string, Handler>;
 
   constructor(options: {
@@ -52,36 +57,138 @@ export class Router extends DiscordInteractions {
       ...userRoutes
     } = options;
 
-    super({
-      applicationId,
-      publicKey,
-      authToken,
-      tokenPrefix,
-    });
+    this.#publicKey = publicKey;
+    this.#applicationId = applicationId;
+    this.#authToken = authToken;
+    this.#tokenPrefix = (tokenPrefix || "Bot") + " ";
 
     const routes: Record<string, Handler>  = {
       [endpoint]: this.#handleInteraction.bind(this) as Handler,
     }
 
-    const commands: Record<string, Handler> = {};
+    const commands: PartialApplicationCommand[] = [];
+    const commandMap: Record<string, Handler> = {};
 
     for (const [route, handler] of Object.entries(userRoutes)) {
-      const url = new URL(route, "https://example.com");
-      // Provides a HTTP route for each commands.
-      // Note: searchParams are ignored.
-      routes[url.pathname] = handler;
-      // Creates application command from the route.
-      const command = this.registerApplicationCommand(url, guildId);
+      // Ensures the handler's name is the route.
+      Object.defineProperty(handler, "name", { value: route, });
 
+      const url = new URL(route, "https://example.com");
+      // Creates application command from the route.
+      const command = this.commandFromUri(url);
       if (command) {
-        commands[command.name] = handler;
+        commands.push(command);
+        // Provides a HTTP route for each commands.
+        // Note: searchParams are ignored.
+        routes[url.pathname] = handler;
+        // Keeps a mapping between command name and route handler.
+        commandMap[command.name] = handler;
       }
+    }
+
+    this.#commands = commandMap;
+
+    this.bulkOverwriteGuildApplicationCommands(commands, guildId).then(_commands => {
+      // Serves HTTP endpoint for Discord to send payload to.
+      serve(router(routes));
+    });
+  }
+
+  /**
+   * Creates a partial application command from route URL.
+   */
+  commandFromUri(uri: string | URL): PartialApplicationCommand | null {
+    /** @TODO: Uses decorators for description and option type? */
+    const { pathname, searchParams } = new URL(uri.toString(), "https://example.com");
+    /** @FIXME: Command name is not always the only one. */
+    const [_, name, ...params] = pathname.split("/");
+
+    if (!NAME_REGEX.test(name)) {
+      console.error(`Invalid command name: ${ name }`);
+      return null;
+    }
+
+    // Required options must be listed before optional options.
+    const options: ApplicationCommandOption[] = [];
+
+    // Params are required options.
+    params.forEach(param => {
+      const name = param.substring(1);
+      options.push({
+        type: ApplicationCommandOptionType.STRING,
+        name,
+        description: name, /** @FIXME: Actual description */
+        required: true,
+        choices: undefined,
+        // channel_types: undefined,
+        options: undefined,
+        // min_value: undefined,
+        // max_value: undefined,
+        // autocomplete: undefined,
+      });
+    });
+
+    // Search params are optional options.
+    searchParams.forEach((_defaultValue, name) => {
+      options.push({
+        type: ApplicationCommandOptionType.STRING,
+        name,
+        description: name, /** @FIXME: Actual description */
+        required: false,
+        choices: undefined,
+        // channel_types: undefined,
+        options: undefined,
+        // min_value: undefined,
+        // max_value: undefined,
+        // autocomplete: undefined,
+      });
+    });
+
+    const command: PartialApplicationCommand = {
+      name,
+      description: name, /** @FIXME: Actual description */
+      options,
+    }
+
+    return command;
+  }
+
+  async bulkOverwriteGuildApplicationCommands(
+    partials: PartialApplicationCommand[],
+    guildId?: Snowflake,
+  ) {
+    const endpoint = guildId
+    ? `applications/${ this.#applicationId }/guilds/${ guildId }/commands`
+    : `applications/${ this.#applicationId }/commands`;
+    const headers = {
+      "Authorization": `${ this.#tokenPrefix }${ this.#authToken }`,
+      "Content-Type": "application/json",
     };
 
-    // Serves HTTP endpoint for Discord to send payload to.
-    serve(router(routes));
+    const response = await fetch(`${ DISCORD_BASE_URL }/${ endpoint }`, {
+      method: "PUT",
+      headers,
+      body: JSON.stringify(partials),
+    });
 
-    this.#commands = commands;
+    const commands = await response.json() as ApplicationCommand[];
+
+    // Deletes application commands when process exits.
+    globalThis.addEventListener("unload", async (_event: Event): Promise<void> => {
+      for await (const command of commands) {
+        const commandId = command.id;
+        const endpoint = guildId
+        ? `applications/${ this.#applicationId }/guilds/${ guildId }/commands/${ commandId }`
+        : `applications/${ this.#applicationId }/commands/${ commandId }`;
+
+        fetch(endpoint, {
+          method: "DELETE",
+          headers,
+        });
+      }
+    });
+
+    return commands;
   }
 
   async #handleInteraction(request: Request, connInfo: ConnInfo): Promise<Response> {
@@ -103,11 +210,11 @@ export class Router extends DiscordInteractions {
       data: {
         // id,
         name,
-        options,
+        options = [],
       } = {
         id: "",
         name: "",
-        options: []
+        options: [],
       } as ApplicationCommandInteractionData,
       // guild_id,
       // channel_id,
@@ -191,8 +298,8 @@ export class Router extends DiscordInteractions {
     });
   }
 
-  /** 
-   * Verify whether the request is coming from Discord. 
+  /**
+   * Verify whether the request is coming from Discord.
    * When the request's signature is not valid, we return a 401 and this is
    * important as Discord sends invalid requests to test our verification.
    */
@@ -205,14 +312,13 @@ export class Router extends DiscordInteractions {
     if (!timestamp) {
       return { error: `header X-Signature-Timestamp not available`, status: 400, };
     }
-    
+
     const body = await request.text();
 
     const valid = nacl.sign.detached.verify(
       new TextEncoder().encode(timestamp + body),
       hexToUint8Array(signature),
-      // @ts-ignore `publicKey` should not be private.
-      hexToUint8Array(this.publicKey),
+      hexToUint8Array(this.#publicKey),
     );
 
     if (!valid) {
@@ -220,69 +326,6 @@ export class Router extends DiscordInteractions {
     }
 
     return { body };
-  }
-
-  registerApplicationCommand(uri: string | URL, guildId?: Snowflake): ApplicationCommand | null {
-    /** @TODO: Uses decorators for description and option type? */
-    const { pathname, searchParams } = new URL(uri.toString(), "https://example.com");
-    /** @FIXME: Command name is not always the only one. */
-    const [_, name, ...params] = pathname.split("/");
-
-    if (!NAME_REGEX.test(name)) {
-      console.error(`Invalid command name: ${ name }`);
-      return null;
-    }
-
-    // Required options must be listed before optional options.
-    const options: ApplicationCommandOption[] = [];
-
-    // Params are required options.
-    params.forEach(param => {
-      const name = param.substring(1);
-      options.push({
-        type: ApplicationCommandOptionType.STRING,
-        name,
-        description: name, /** @FIXME: Actual description */
-        required: true,
-        choices: undefined,
-        // channel_types: undefined,
-        options: undefined,
-        // min_value: undefined,
-        // max_value: undefined,
-        // autocomplete: undefined,
-      });
-    });
-
-    // Search params are optional options.
-    searchParams.forEach((_defaultValue, name) => {
-      options.push({
-        type: ApplicationCommandOptionType.STRING,
-        name,
-        description: name, /** @FIXME: Actual description */
-        required: false,
-        choices: undefined,
-        // channel_types: undefined,
-        options: undefined,
-        // min_value: undefined,
-        // max_value: undefined,
-        // autocomplete: undefined,
-      });
-    });
-
-    const command: PartialApplicationCommand = {
-      name,
-      description: name, /** @FIXME: Actual description */
-      options,
-    }
-
-    this.createApplicationCommand(command, guildId).then(command => {
-      // Deletes application command when process exits.
-      globalThis.addEventListener("unload", (_event: Event): void => {
-        this.deleteApplicationCommand(command.id, guildId);
-      });
-    });
-
-    return command as ApplicationCommand;
   }
 }
 
