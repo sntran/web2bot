74259c4fca8c5799ec2e300edb313fe48e76bcde
Son Tran-Nguyen <github@sntran.com>
2023-03-08 17:58:38 -0600

    fix: use esm.sh for more compatibily

--- deps.ts
+++ deps.ts
@@ -2,7 +2,7 @@ export type { ConnInfo } from "https://deno.land/std@0.177.0/http/server.ts";
 
 // TweetNaCl is a cryptography library that we use to verify requests
 // from Discord.
-export { verify } from "npm:watsign@0.1";
+export { verify } from "https://esm.sh/watsign@0.1.8";
 
 export {
   ApplicationCommandOptionType,
